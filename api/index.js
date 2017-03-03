a9dbf43c5251292865d488645c70a27cdc8185fe
Theo Tison <tison.theo@gmail.com>
2017-03-03 13:33:52 +0100

    1st route register + api REST

--- api/index.js
+++ api/index.js
@@ -1,17 +1,58 @@
-const express = require('express');
-const app = express();
 const path = require('path');
-const api = require('./routes/routes');
+var express = require("express");
+var mysql   = require("mysql");
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+var rest = require("./routes/routes.js");
+var app  = express();
 
-var bodyParser = require('body-parser');
+function REST(){
+    var self = this;
+    self.connectMysql();
+};
 
-app.set('view engine', 'jade');
-app.use(express.static(path.join('node_modules')));
-app.use("/", express.static(path.join('./front')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+REST.prototype.connectMysql = function() {
+    var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 100,
+        host     : 'localhost',
+        user     : 'root',
+        password : 'root',
+        database : 'pli',
+        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+        debug    :  false
+    });
+    pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+        }
+    });
+}
 
-app.use('/api', api);
+REST.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.set('view engine', 'jade');
+      app.use(express.static(path.join('node_modules')));
+      app.use("/", express.static(path.join('./front')));
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new rest(router,connection,md5);
+      self.startServer();
+}
 
-app.listen(3000);
-console.log('server is running');
+REST.prototype.startServer = function() {
+      app.listen(3000,function(){
+          console.log("All right ! I am alive at Port 3000.");
+      });
+}
+
+REST.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL n" + err);
+    process.exit(1);
+}
+
+new REST();
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const router = express.Router();
+var mysql = require('mysql');
+function REST_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
 
-/* GET api listing. */
-router.get('/test', (req, res) => {
-  res.send('api works');
-});
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+    router.get("/",function(req,res){
+        res.json({ "Message" : "Hello World !" });
+    });
+    router.post("/register",function(req,res){
+      var query = "INSERT INTO ??(??,??,??,??) VALUES (?,?,?,?)";
+      var table = ["user","name","mail","roles","password",req.body.name,req.body.mail,req.body.roles,md5(req.body.password)];
+      query = mysql.format(query,table);
+      connection.query(query,function(err,rows){
+          if(err) {
+              res.json({"Error" : 400, "Message" : "Error executing MySQL query"});
+          } else {
+              res.json({"Error" : 200, "User" : {
+                  "lastname": req.body.name,
+                  "email": req.body.mail,
+                  "password": req.body.password,
+                  "role": req.body.roles
+              }});
+          }
+      });
+  });
+}
 
-module.exports = router;
+module.exports = REST_ROUTER;
