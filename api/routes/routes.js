d17503171808ee2572c2cade3e606a02468e9247
Theo Tison <tison.theo@gmail.com>
2017-03-22 15:56:07 +0100

    debug rebase

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,26 +1,12 @@
 var mysql = require('mysql');
 
-var google = require("googleapis");
-var OAuth2 = google.auth.OAuth2;
-var oauth2Client = new OAuth2('789981830396-ht9j545usj60vsqgpdccran0l6ghvmgv.apps.googleusercontent.com', 'Uge7KgC1gS0OTo1OsM1PyJYR', 'http://localhost:3000/api');
-
-var scopes = [
-  'https://www.googleapis.com/auth/drive'
-];
-var url = oauth2Client.generateAuthUrl({
-  access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
-  scope: scopes // If you only need one scope you can pass it as string
-});
-
-
-
 function REST_ROUTER(router,connection,md5) {
     var self = this;
     self.handleRoutes(router,connection,md5);
 }
 
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
-    
+
     // API RESGISTER
     router.post("/register",function(req,res){
       var query = "INSERT INTO ??(??,??,??,??) VALUES (?,?,?,?)";
@@ -59,17 +45,6 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
   });
   // END API LOGIN
 
-// ROUTE TEST 
- router.post ("/test", function(req, res){
-  // //test GOOGLE
-
-  
-  res.send(url);
-
-  //test GOOGLE
-
- });
-// END ROUTE TEST 
 }
 
 module.exports = REST_ROUTER;
