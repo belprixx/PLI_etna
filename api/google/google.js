5f470d35fa2cbc51e3771f8d9317c369875fc6c2
SFatier <fatier_s@etna-alternance.net>
2017-03-24 01:11:31 +0100

    api google

--- api/google/google.js
+++ api/google/google.js
@@ -1,6 +1,13 @@
 var google = require("googleapis");
 var OAuth2 = google.auth.OAuth2;
 var oauth2Client = new OAuth2('789981830396-ht9j545usj60vsqgpdccran0l6ghvmgv.apps.googleusercontent.com', 'Uge7KgC1gS0OTo1OsM1PyJYR', 'http://localhost:3000/api');
+var scopes = [
+  'https://www.googleapis.com/auth/drive'
+];
+var url = oauth2Client.generateAuthUrl({
+  access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+  scope: scopes // If you only need one scope you can pass it as string
+});
 
 function REST_GOOGLE(router,connection,md5) {
     var self = this;
@@ -9,42 +16,34 @@ function REST_GOOGLE(router,connection,md5) {
 
 REST_GOOGLE.prototype.handleRoutes= function(router,connection,md5) {
 
+// ROUTE TEST
+     router.post ("/google/test", function(req, res){
+         console.log("Google");
+         res.json({
+        	"Error": 200,
+         });
+     });
+    // END ROUTE TEST
+
+    router.post("/google/url", function(req, res) {
+	    res.send(url);
+	    res.status(200);
+	});
+
+    // ROUTE google oauthRedirect 
+    router.get("/google/oauth2callback", function(req, res){
+         
+	    var code = req.query.code;
+
+	     oauth2Client.getToken(code, function(err, tokens) {
+	      if (err) {
+	        res.send(err);
+	        return;
+	      }      
+	      oauth2Client.setCredentials(tokens);
+	      res.send(tokens);
+	    });
+     });
 }
-
-
-/*
-    SEGO FAUT REPASSER SUR LA PARTIE COMMENTER
-    Regarde DropBox pour bien découper
-    Slack si bession + skype si tu arrive pas
-*/
-
-
-// var scopes = [
-//   'https://www.googleapis.com/auth/drive'
-// ];
-// var url = oauth2Client.generateAuthUrl({
-//   access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
-//   scope: scopes // If you only need one scope you can pass it as string
-// });
-//
-// REST_GOOGLE.prototype.connectGoogleDrive = function (err){
-//   var OAuth2 = google.auth.OAuth2;
-//
-//   var oauth2Client = new OAuth2("532417609984-6grimhtlrblhkrjgkvmi1okem6pkfhs2.apps.googleusercontent.com", "JrSHP3fMBl-9CuZkGZmrgB2f", "http://localhost:3000/api/googleCallBack");
-//
-//   // generate a url that asks permissions for Google+ and Google Calendar scopes
-//   var scopes = [
-//     'https://www.googleapis.com/auth/gmail.modify'
-//   ];
-//
-//   var url = oauth2Client.generateAuthUrl({
-//     access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
-//     scope: scopes // If you only need one scope you can pass it as string
-//   });
-//
-//   app.get("/url", function(req, res) {
-//     res.send(url);
-//   });
-// }
 
 module.exports = REST_GOOGLE;
--- front/controller/homeController.js
+++ front/controller/homeController.js
@@ -5,24 +5,38 @@ angular.module('pliApp').controller('homeController', function($scope, $location
     $scope.lastName = userName.lastName;
 
     var wtop;
-	$scope.myFunc = function() {
+	  $scope.myFunc = function() {
         console.log("clik");
-        $http({method: 'POST', url: '/api/test'}).
-		  then(function(response) {
+        $http({method: 'POST', url: '/api/google/url'}).then(function(response) {
             console.log(response);
-		  	if(response.status === 200) {
-		  		console.log(response.status);
-               wtop = $window.open(response.data, "toto", "width:500px,height:700px");
-		  	} else
-		  	      {
-		  			 console.log("Error");
-		  		  }
-		  });
+		  	  if(response.status === 200) {
+		  		  console.log(response.status);
+            wtop = $window.open(response.data, "Cliquer ici!!", "width:500px,height:700px");
+		  	  }else{
+		  		  console.log("Error");
+		  	  }
+		    });
 
-          window.onmessage = function(e){
+        window.onmessage = function(e){
+          wtop.close();
+          var url = e.data;
+          console.log(wtop);
+
+          if(wtop){
             wtop.close();
-            var url = e.data;
-            console.log(wtop);
           }
+        var url = e.data;        
+        var idx = urlWithCode.lastIndexOf("code=");
+        var code = urlWithCode.substring(idx + 5).replace("#","");
+        var url = "/api/google/oauth2callback?code=" + code;
+        //ouverture de la deuxième page google !!! ajouter dans console developper
+        $http.get(url).then(function(response) { console.log("token ok..");  });
+
+        $http.get("https://www.googleapis.com/drive/v2/files/").then(function(response){
+          console.log(response);
+        });
+
+
+        }
     };
 });
