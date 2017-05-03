aae770ca40b3982536d643c418c26964af70a8df
Théo Tison <tison.theo@gmail.com>
2017-05-03 15:20:24 +0200

    dump modify

--- dumpBdd.sql
+++ dumpBdd.sql
@@ -2,22 +2,22 @@
 -- version 4.4.10
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Mar 03, 2017 at 10:58 AM
--- Server version: 5.5.42
--- PHP Version: 7.0.8
+-- Client :  localhost:3306
+-- Généré le :  Mer 29 Mars 2017 à 13:20
+-- Version du serveur :  5.5.42
+-- Version de PHP :  7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `pli`
+-- Base de données :  `pli`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cloud`
+-- Structure de la table `cloud`
 --
 
 CREATE TABLE `cloud` (
@@ -29,7 +29,7 @@ CREATE TABLE `cloud` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `token`
+-- Structure de la table `token`
 --
 
 CREATE TABLE `token` (
@@ -41,56 +41,65 @@ CREATE TABLE `token` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Firstname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Lastname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `mail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `roles` enum('admin','user') COLLATE utf8_unicode_ci NOT NULL,
-  `id_token` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `roles` enum('user','admin') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `Firstname`, `Lastname`, `mail`, `password`, `roles`) VALUES
+(1, 'theo', 'tison', 'theoTest@gmail.com', '098f6bcd4621d373cade4e832627b4f6', 'user'),
+(2, 'theo', 'test', 'theo@gmail.com', 'f71dbe52628a3f83a77ab494817525c6', 'admin'),
+(3, 'theo', 'tison', 'test@test.fr', '813ffe9e5f06a3a119e33e554de38e46', 'user');
+
+--
+-- Index pour les tables exportées
 --
 
 --
--- Indexes for table `cloud`
+-- Index pour la table `cloud`
 --
 ALTER TABLE `cloud`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `token`
+-- Index pour la table `token`
 --
 ALTER TABLE `token`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Index pour la table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT for table `cloud`
+-- AUTO_INCREMENT pour la table `cloud`
 --
 ALTER TABLE `cloud`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `token`
+-- AUTO_INCREMENT pour la table `token`
 --
 ALTER TABLE `token`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
